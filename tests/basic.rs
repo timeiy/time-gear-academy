161dfbadca8c51a27999d13a457f614d6930ff65
timeiy <timeoutti@hotmail.com>
2024-12-04 11:32:48 +0800

    a

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,37 +1,99 @@
-use gtest::{Program, System};
-use pebbles_game_io::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use gtest::{Log, Program, System};
 
-#[test]
-fn basic_initialization() {
-    let sys = System::new();
-    sys.init_logger();
-    let program = Program::current(&sys);
+    #[test]
+    fn test_init() {
+        let sys = System::new();
+        let program = Program::current(&sys);
 
-    // 测试初始化
-    let init_config = PebblesInit {
-        difficulty: DifficultyLevel::Easy,
-        pebbles_count: 15,
-        max_pebbles_per_turn: 2,
-    };
-    let res = program.send(2, init_config);
-    assert!(!res.main_failed());
+        let init_config = PebblesInit {
+            pebbles_count: 10,
+            max_pebbles_per_turn: 3,
+            difficulty: DifficultyLevel::Easy,
+        };
+
+        let res = program.send(2, init_config);
+        assert!(!res.main_failed());
+    }
+
+    #[test]
+    fn test_user_turn() {
+        let sys = System::new();
+        let program = Program::current(&sys);
+
+        let init_config = PebblesInit {
+            pebbles_count: 10,
+            max_pebbles_per_turn: 3,
+            difficulty: DifficultyLevel::Easy,
+        };
+        program.send(2, init_config);
+
+        let res = program.send(2, PebblesAction::Turn(2));
+        assert!(!res.main_failed());
+    }
+
+    #[test]
+    fn test_game_win() {
+        let sys = System::new();
+        let program = Program::current(&sys);
+
+        let init_config = PebblesInit {
+            pebbles_count: 3,
+            max_pebbles_per_turn: 3,
+            difficulty: DifficultyLevel::Easy,
+        };
+        program.send(2, init_config);
+
+        let res = program.send(2, PebblesAction::Turn(3));
+        let log = Log::builder()
+            .source(program.id())
+            .dest(2)
+            .payload_bytes(PebblesEvent::Won(Player::User));
+        assert!(res.contains(&log));
+    }
+
+    #[test]
+    fn test_give_up() {
+        let sys = System::new();
+        let program = Program::current(&sys);
+
+        let init_config = PebblesInit {
+            pebbles_count: 10,
+            max_pebbles_per_turn: 3,
+            difficulty: DifficultyLevel::Easy,
+        };
+        program.send(2, init_config);
+
+        let res = program.send(2, PebblesAction::GiveUp);
+        let log = Log::builder()
+            .source(program.id())
+            .dest(2)
+            .payload_bytes(PebblesEvent::Won(Player::Program));
+        assert!(res.contains(&log));
+    }
+
+    #[test]
+    fn test_restart() {
+        let sys = System::new();
+        let program = Program::current(&sys);
+
+        let init_config = PebblesInit {
+            pebbles_count: 10,
+            max_pebbles_per_turn: 3,
+            difficulty: DifficultyLevel::Easy,
+        };
+        program.send(2, init_config);
+
+        let res = program.send(
+            2,
+            PebblesAction::Restart {
+                pebbles_count: 15,
+                max_pebbles_per_turn: 4,
+                difficulty: DifficultyLevel::Hard,
+            },
+        );
+        assert!(!res.main_failed());
+    }
 }
-
-#[test]
-fn test_game_flow() {
-    let sys = System::new();
-    let program = Program::current(&sys);
-
-    // 初始化游戏
-    let init_config = PebblesInit {
-        difficulty: DifficultyLevel::Easy,
-        pebbles_count: 15,
-        max_pebbles_per_turn: 2,
-    };
-    program.send(2, init_config);
-
-    // 测试玩家回合
-    let res = program.send(2, PebblesAction::Turn(2));
-    assert!(!res.main_failed());
-}
-
